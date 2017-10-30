f761cebbb9acf59d7a24fc7fc37ec93e6e3deffa
Dana Visescu <dana.visescu@rxpservices.com>
2017-10-31 09:22:57 +1100

    added comments

--- content/5waysofwellbeing/adapt/js/scriptLoader.js
+++ content/5waysofwellbeing/adapt/js/scriptLoader.js
@@ -16,7 +16,39 @@ Modernizr.load([
         test: Modernizr.video || Modernizr.audio,
         nope: 'libraries/swfObject.js',
         complete: function() {
-            yepnope.injectJs("adapt/js/adapt.min.js");
+            yepnope.injectJs("adapt/js/adapt.min.js", function ()
+            {
+                $(function () {
+                    $("#addComment").click(function () {
+                        $('#divAddComment').slideToggle();
+                        //$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                        return false;
+                    });
+
+                    $("#showComments").click(function () {
+                        $('#listComments').slideToggle();
+                        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                        if ($("#showComments > span").text() == 'SHOW COMMENTS')
+                            $("#showComments > span").text('HIDE COMMENTS');
+                        else
+                            $("#showComments > span").text('SHOW COMMENTS');
+                        return false;
+                    });
+                    
+                    $("#postComment").click(function () {
+                        alert('Comment posted ' + $('#comment').val());
+                        return false;
+                    });
+
+                    $('#customComponent').slideToggle();
+                    $('#divAddComment').slideToggle();
+                    $('#listComments').slideToggle();
+
+                    $('#noViews').text('512');
+                    $('#noLikes').text('43');
+                    $('#noComments').text('66');
+                });}
+            );
         }
     }
 ]);
